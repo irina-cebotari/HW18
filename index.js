eeef92a17ec44a19dabc7396cde07118ecf4ec92
irina-cebotari <irina.cebotari0301@gmail.com>
2024-03-19 22:39:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// ex1
+
+// const arr = [5, 30, 15, 40, 50, 25, 10, 35, 20, 45]
+
+// const containsNum = arr.every((item) => typeof item === 'number')
+
+// console.log(containsNum)
+
+// if(!containsNum) {
+//     console.log('va rog sa introduceti doar liste de cifre') 
+// }
+
+// console.log(arr)
+
+// arr.sort((a, b) => a - b);
+// console.log(arr);
+
+// arr.sort((a, b) => b - a);
+// console.log(arr);
+
+
+
+// ex2
+
+// const arr = [1, 2, 3, 4, 'ceva scris', 5, 6, 7, 8]
+
+// const indexOfItem = arr.indexOf('ce scris')
+
+// console.log(indexOfItem)
+
+// if(indexOfItem === -1) {
+//     console.log('not found') 
+// } 
+
+
+
+// ex3
+
+// const arr = [2, 4, 6, 9, 36, 121]
+
+// const arrSqrt = arr.filter(num => Number.isInteger(Math.sqrt(num)))
+
+// console.log(arrSqrt)
+
+
+
+// ex4
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+// const containsNum = arr.every((item) => typeof item === 'number')
+// const evenNum = arr.filter(item => item % 2 === 0)
+// const oddNum = arr.filter(item => item % 2 !== 0)
+
+// console.log(containsNum)
+// console.log(evenNum)
+// console.log(oddNum)
